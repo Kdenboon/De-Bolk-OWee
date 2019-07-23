60ed112f6c9e759e024253084fdd47ad5cd691ca
Kevin <k.denboon@student.tudelft.nl>
2019-07-23 10:48:41 +0200

    Workig sponsorbar

--- pages/sponsor.vue
+++ pages/sponsor.vue
@@ -3,86 +3,31 @@
     <nav class="navbar is-fixed-bottom" role="navigation" aria-label="main navigation">
       <div class="navbar-menu is-active"
            v-bind:class="{ 'is-down': burger, 'navmenusmall': ($mq !== 'threecards' && $mq !== 'fourcards' && $mq !== 'desktop') }">
-        <div v-if="$cookie.get('language') == 'dutch'" class="navbar-start">
           <div class="menuitemcontainer"><a href="https://info.ogd.nl/" target="_blank"><img src="~assets/sponsors/ogd.jpg" class="small-image"/></a></div>
           <div class="menuitemcontainer"><a href="https://www.stud.nl/" target="_blank"><img src="~assets/sponsors/stud.svg" class="small-image"/></a></div>
           <div class="menuitemcontainer"><a href="https://www.lunatech.com/" target="_blank"><img src="~assets/sponsors/lunatech.png" class="small-image"/></a></div>
-        </div>
-
-        <div v-if="$cookie.get('language') == 'english'" class="navbar-start">
-          <div class="menuitemcontainer"><a href="#imageheader" v-smooth-scroll @click="burger = false">Home</a></div>
-          <div class="menuitemcontainer"><a href="#main" v-smooth-scroll @click="burger = false">OWee</a></div>
-          <div class="menuitemcontainer"><a href="#vereniging" v-smooth-scroll @click="burger = false">Association</a></div>
-          <div class="menuitemcontainer"><a href="#schema" v-smooth-scroll @click="burger = false">Schedule</a></div>
-          <div class="menuitemcontainer"><a href="#kmt" v-smooth-scroll @click="burger = false">KMT</a></div>
-          <div class="menuitemcontainer"><a href="#sleepin" v-smooth-scroll @click="burger = false">Sleep-Inn</a></div>
-          <div class="menuitemcontainer"><a href="#quotes" v-smooth-scroll @click="burger = false">Quotes</a></div>
-          <div class="menuitemcontainer"><a href="#contact" v-smooth-scroll @click="burger = false">Contact</a></div>
-        </div>
+          <div class="menuitemcontainer"><a href="https://www.makro.nl/" target="_blank"><img src="~assets/sponsors/makro.png" class="small-image"/></a></div>
+          <div class="menuitemcontainer"><a href="https://www.printerpro.nl/home/" target="_blank"><img src="~assets/sponsors/printerpro.jpg" class="small-image"/></a></div>
+          <div class="menuitemcontainer"><a href="https://www.tradecloud1.com/nl/" target="_blank"><img src="~assets/sponsors/tradecloud.png" class="small-image"/></a></div>
       </div>
     </nav>
   </div>
 </template>
 
-<script>
-
-  export default{
-    data(){
-      return {
-        burger: false
-      }
-    },
-
-    methods: {
-      setCookie(lang){
-        //set cookie for 100 days, that should probably be enough
-        this.$cookie.set('language', lang, '100')
-        //this.burger = false;
-        location.reload()
-      }
-    }
-  }
-
-
-
-</script>
-
-<style>
+<style scoped>
 
   .small-image{
-    max-height: 60px;
-  }
-
-  .navbar{
-    background-color: #0d8141;
-  }
-
-  .flag{
-    width: 25px;
-    position: relative;
-    top: 6px;
+    max-height: 50px;
   }
 
   .navbar-menu{
-    background-color: #0d8141;
+    background-color: white;
     text-align: center;
     width: 100%;
     padding: 0;
     position: fixed;
-    border-bottom: 5px solid #f5eb22;
+    border-top: 5px solid #f5eb22;
     transition: 0.2s ease all;
-  }
-
-  .navmenusmall{
-    top: -450px;
-  }
-
-  .is-down{
-    top: 57px;
-  }
-
-  .navbar-brand{
-    border-bottom: 5px solid #f5eb22;
   }
 
   .navbar-menu a {
@@ -96,21 +41,7 @@
     transition: 0.2s ease all;
     border-radius: 10px;
     text-align: center;
-    padding: 0 20px;
-  }
-
-  .menuitemcontainer:hover {
-    background-color: white;
-  }
-
-  .menuitemcontainer:hover a{
-    color: #0d8141;
-  }
-
-  .navbar a{
-    font-family: 'Komikax', cursive;
-    color: white;
-    font-size: 1.2rem;
+    padding: 0px 4%;
   }
 
   .navbar-brand img{
